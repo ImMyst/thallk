1d5ebffad158808ee051425af21a396214e6454f
ImMyst <titouan.galvani@yeap.ai>
2023-11-26 11:10:30 +0100

    chore: add pandacss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import pandacss from '@pandacss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), pandacss()],
   output: 'hybrid',
 });
