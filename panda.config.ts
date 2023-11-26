4c3febafdcb0a453b654f2f1ce5b2b74b3df4d71
ImMyst <titouan.galvani@yeap.ai>
2023-11-26 16:22:13 +0100

    style: add nohemi font

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,13 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        fonts: {
+          nohemi: { value: 'var(--font-nohemi), sans-serif' },
+        }
+      }
+    },
   },
 
   // The output directory for your css system
